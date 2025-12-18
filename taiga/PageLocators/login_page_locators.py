64359e84fc86a4db1464456343a81826d61a9b14
chenwengang <chenwengang@cppe.com.cn>
2025-12-18 17:22:05 +0800

    补充登录元素定位文件

---
+++ taiga/PageLocators/login_page_locators.py
@@ -0,0 +1,25 @@
+# @Create Date: 2025/6/18
+# @Author: ganlu
+from selenium.webdriver.common.by import By
+
+
+class LoginPageLocators:
+    """
+    登录页面元素定位
+    """
+    # 用户名
+    username = [By.XPATH, "//input[@placeholder='用户名']"]
+    # 密码
+    password = [By.XPATH, "//input[@placeholder='密码']"]
+    # 验证码输入框
+    verify_code = [By.XPATH, "//input[@placeholder='验证码']"]
+    # 验证码图片
+    verify_code_img = [By.CLASS_NAME, "el-image__inner"]
+    # 登录
+    login_buton = [By.XPATH, "//span[text()='登 录']/parent::button"]
+    # 账号在其他设备登录，确定登录
+    login_confirm = [By.XPATH, "//span[text()='确定']/parent::button"]
+    # 未输入用户名或密码提示
+    no_message = [By.XPATH, "//div[contains(@class,'el-form-item__error')]"]
+    # 提示信息框
+    message = [By.XPATH, "//p[@class='el-message__content']"]
